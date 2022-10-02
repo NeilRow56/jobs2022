05aad04d0c6653f1cc4efcf832a00db95a37c901
Neil Rowland <neilrowland56@yahoo.com>
2022-10-02 06:28:33 +0100

    Amended Home Page so that convertDocToObj within index.js and not dbConnect

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,27 @@ export default function Index({ jobs }) {
    <JobsList jobs = {jobs} />
   );
 }
+// export async function getStaticProps() {
+//   await db.Connect();
+//   const jobs = await Job.find().lean();
+//   return {
+//     props: {
+//       jobs: jobs.map(db.convertDocToObj),
+//     },
+//   };
+// }
 export async function getStaticProps() {
   await db.Connect();
-  const jobs = await Job.find().lean();
-  return {
-    props: {
-      jobs: jobs.map(db.convertDocToObj),
-    },
-  };
-}
+
+   /* find all the data in our database */
+   const result = await Job.find({})
+   const jobs = result.map((doc) => {
+    const job = doc.toObject()
+    job._id = job._id.toString()
+    job.datePosted = job.datePosted.toString();  
+    job.updatedAt = job.updatedAt.toString();
+     return job
+   })
+ 
+   return { props: { jobs: jobs } }
+ }
